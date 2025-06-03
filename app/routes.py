c2fb6ae5262c84c0f2e3d534445749efcb24b1e6
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-06-04 00:26:55 +0100

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .models import User
+from . import db
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@main_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid credentials')
+    return render_template('login.html')
+
+@main_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@main_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('User already exists')
+            return redirect(url_for('main.register'))
+
+        new_user = User(username=username, password=generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
+        flash('User created, please login')
+        return redirect(url_for('main.login'))
+    return render_template('register.html')
